9e6cb57a48ad002efb7b1c77d2b768a33b3aa3fe
dembag <jn.denby@gmail.com>
2026-08-14 09:01:38 +0200

    Created routes for index and post_users

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from data_manager import DataManager
 from models import Base, engine, Movie
 
 app = Flask(__name__)
+dm = DataManager()
 
 @app.route("/")
-def home():
-    return "Welcome to MovieWeb App!"
+def index():
+    users = dm.get_users()
+    return render_template("index.html", users=users)
 
 
+@app.route("/users", methods=["POST"])
+def create_user():
+    users = dm.get_users()
+    return str(users)               # Temporarily return users as a string
 
 
 if __name__ == "__main__":
